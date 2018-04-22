aa038480baf2ed44a1a003c74d5658f09dba8c59
levady <levady@tradegecko.com>
2018-04-22 11:37:37 +0800

    [ENHANCEMENT] Set root route and force all request format to JSON.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  before_action :set_default_response_format
+
+  def root
+    render json: { message: "Nothing to see here, move along." }
+  end
+
+protected
+
+  def set_default_response_format
+    request.format = :json
+  end
 end
