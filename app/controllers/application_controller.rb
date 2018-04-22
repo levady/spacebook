60a159a11a2f3fc4898ed90e3b784d96e53cef27
levady <levady@tradegecko.com>
2018-04-22 17:14:46 +0800

    [REFACTOR] Make "render_error" can handle exception error to split out a proper error response.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::API
   before_action :set_default_response_format
   before_action :validate_params
 
+  rescue_from ActiveRecord::RecordInvalid, :with => :render_error
+
   def root
     render json: { message: "Nothing to see here, move along." }
   end
@@ -15,6 +17,7 @@ protected
   def validate_params; end
 
   def render_error(object)
-    render json: { success: false, errors: object.errors }
+    errors = object.is_a?(StandardError) ? object.record.errors : object.errors
+    render json: { success: false, errors: errors }
   end
 end
