9ff64faad274c799cc3b78a1dcae574f928d07b0
levady <levady@tradegecko.com>
2018-04-22 14:28:35 +0800

    [ENHANCEMENT] Add an optional step to validate params in controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :set_default_response_format
+  before_action :validate_params
 
   def root
     render json: { message: "Nothing to see here, move along." }
@@ -9,5 +10,11 @@ protected
 
   def set_default_response_format
     request.format = :json
+  end
+
+  def validate_params; end
+
+  def render_error(object)
+    render json: { success: false, errors: object.errors }
   end
 end
